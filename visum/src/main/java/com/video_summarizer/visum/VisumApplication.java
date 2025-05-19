7d75e72d2ba40cd2c8dd0455abe873fe0a4c3dba
CookiePie-cell <juliojnicolas1@gmail.com>
2025-05-19 10:19:23 +0700

    init project

---
+++ visum/src/main/java/com/video_summarizer/visum/VisumApplication.java
@@ -0,0 +1,13 @@
+package com.video_summarizer.visum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisumApplication.class, args);
+	}
+
+}
---
+++ visum/src/test/java/com/video_summarizer/visum/VisumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.video_summarizer.visum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
