e002324fc48d423211406b29a70cf457b5af1153
CookiePie-cell <juliojnicolas1@gmail.com>
2025-05-20 08:51:51 +0700

    setting up docker and rabbit config

---
+++ visum/src/main/java/com/video_summarizer/visum/config/RabbitConfig.java
@@ -0,0 +1,34 @@
+package com.video_summarizer.visum.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    static final String QUEUE_NAME = "visum.transcription.queue";
+    static final String EXCHANGE_NAME = "visum.exchange";
+    static final String ROUTING_KEY = "visum.transcription";
+
+    @Bean
+    public Queue transcribeQueue() {
+        return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(ROUTING_KEY);
+    }
+}
